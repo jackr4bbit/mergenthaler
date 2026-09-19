bfd8f16e8a57a6bd94d46d048f231c78a8dddd82
Jack Huey <jhhuey@icloud.com>
2026-09-19 09:00:10 -0500

    Fix `__all__` mixing strings and classes

--- src/mergenthaler/__init__.py
+++ src/mergenthaler/__init__.py
@@ -2,4 +2,4 @@ from .files import * # noqa: F403, F401
 from .markup import * # noqa: F403, F401
 from .site import * # noqa: F403, F401
 
-__all__ = ["File", "Post", "Author", "Feed", "Markup", "Element", "Format", "SimpleFormat", "Site"] + defaultElements + list(defaultFormats)
+__all__ = ["File", "Post", "Author", "Feed", "Markup", "Element", "Format", "SimpleFormat", "Site"] + [c.__name__ for c in defaultElements + list(defaultFormats)]
