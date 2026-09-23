225b3a4e20e48d9f5ad82ebed9e34414114891f0
Jack Huey <jhhuey@icloud.com>
2026-09-23 17:02:25 -0500

    Expose build and serve commands

--- src/mergenthaler/__init__.py
+++ src/mergenthaler/__init__.py
@@ -1,5 +1,6 @@
+from .__main__ import build, serve # noqa: F403, F401
 from .files import * # noqa: F403, F401
 from .markup import * # noqa: F403, F401
 from .site import * # noqa: F403, F401
 
-__all__ = ["File", "Post", "Author", "Feed", "Markup", "Element", "Format", "SimpleFormat", "Site"] + [c.__name__ for c in defaultElements + list(defaultFormats)]
+__all__ = ["build", "serve", "File", "Post", "Author", "Feed", "Markup", "Element", "Format", "SimpleFormat", "Site"] + [c.__name__ for c in defaultElements + list(defaultFormats)]
--- src/mergenthaler/__main__.py
+++ src/mergenthaler/__main__.py
@@ -2,11 +2,69 @@ from pathlib import Path
 import shutil
 import argparse
 from flask import Flask, send_file
-from waitress import serve
+import waitress
 import requests
 
 from .files import Feed
 from .utils import validUrl
+
+def build(feed: Feed, output: Path, nodelete: bool = False):
+    if not nodelete and output.exists():
+        for item in output.iterdir():
+            if item.is_file() or item.is_symlink():
+                item.unlink()
+            elif item.is_dir():
+                shutil.rmtree(item)
+
+    for path, contents in feed.siteTheme(feed).files():
+        path = output / path
+        path.parent.mkdir(parents=True, exist_ok=True)
+        if isinstance(contents, Path):
+            contents.copy(path, preserve_metadata=True)
+        elif isinstance(contents, str):
+            if validUrl(contents):
+                with path.open(mode="wb") as file:
+                    file.write(requests.get(contents).content)
+            else:
+                with path.open(mode="w") as f:
+                    f.write(contents)
+
+def serve(location: Path, port: int = 8080, host: str = "0.0.0.0"):
+    app = Flask(__name__)
+
+    @app.route("/", defaults={"subpath": ""}, methods=["GET"])
+    @app.route("/<path:subpath>", methods=["GET"])
+    def route(subpath: str):
+        def getFile(path: str) -> str | Path | None:
+            return next((file[1] for file in site.files() if file[0] == path), None)
+
+        if subpath.endswith("/"):
+            subpath = subpath[:-1]
+
+        feed = Feed.parse(location)
+        site = feed.siteTheme(feed)
+
+        if subpath == "":
+            file = getFile("index.html")
+        else:
+            file = getFile(subpath)
+            for suffix in [".html", "/index.html"]:
+                if file is not None:
+                    break
+                file = getFile(subpath+suffix)
+
+        if file is None:
+            return site.notFoundPage, 404
+        elif isinstance(file, Path):
+            return send_file(file), 200, ({"Content-Type": "text/css"} if file.suffix == ".css" else None)
+        else:
+            if validUrl(file):
+                response = requests.get(file)
+                return response.content, 200, {"Content-Type": response.headers.get("Content-Type")}
+            else:
+                return file, 200, ({"Content-Type": "text/css"} if subpath.endswith(".css") else None)
+
+    waitress.serve(app, host=host, port=port)
 
 def main():
     parser = argparse.ArgumentParser(description="The Mergenthaler validator, builder, and server.")
@@ -41,63 +99,11 @@ def main():
             else:
                 outputPath = Path.cwd() / "output"
 
-            if not args.nodelete and outputPath.exists():
-                for item in outputPath.iterdir():
-                    if item.is_file() or item.is_symlink():
-                        item.unlink()
-                    elif item.is_dir():
-                        shutil.rmtree(item)
-
-            for path, contents in feed.siteTheme(feed).files():
-                path = outputPath / path
-                path.parent.mkdir(parents=True, exist_ok=True)
-                if isinstance(contents, Path):
-                    contents.copy(path, preserve_metadata=True)
-                elif isinstance(contents, str):
-                    if validUrl(contents):
-                        with path.open(mode="wb") as file:
-                            file.write(requests.get(contents).content)
-                    else:
-                        with path.open(mode="w") as f:
-                            f.write(contents)
+            build(feed, outputPath, args.nodelete)
 
         case "serve":
-            app = Flask(__name__)
-
-            @app.route("/", defaults={"subpath": ""}, methods=["GET"])
-            @app.route("/<path:subpath>", methods=["GET"])
-            def route(subpath: str):
-                def getFile(path: str) -> str | Path | None:
-                    return next((file[1] for file in site.files() if file[0] == path), None)
-
-                if subpath.endswith("/"):
-                    subpath = subpath[:-1]
-
-                feed = Feed.parse(inputPath)
-                site = feed.siteTheme(feed)
-
-                if subpath == "":
-                    file = getFile("index.html")
-                else:
-                    file = getFile(subpath)
-                    for suffix in [".html", "/index.html"]:
-                        if file is not None:
-                            break
-                        file = getFile(subpath+suffix)
-
-                if file is None:
-                    return site.notFoundPage, 404
-                elif isinstance(file, Path):
-                    return send_file(file), 200, ({"Content-Type": "text/css"} if file.suffix == ".css" else None)
-                else:
-                    if validUrl(file):
-                        response = requests.get(file)
-                        return response.content, 200, {"Content-Type": response.headers.get("Content-Type")}
-                    else:
-                        return file, 200, ({"Content-Type": "text/css"} if subpath.endswith(".css") else None)
-
             print(f"Starting server at {args.host}:{args.port}!")
-            serve(app, host=args.host, port=args.port)
+            serve(inputPath, args.port, args.host)
 
         case "test":
             print("Successful!")
